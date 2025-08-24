b021af764a43087b0b92bef761bc5932da11a419
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2025-08-24 10:26:14 -0500

    Improve formatting of simpletest

--- examples/usb_host_descriptor_parser_simpletest.py
+++ examples/usb_host_descriptor_parser_simpletest.py
@@ -5,16 +5,13 @@ import usb.core
 
 from relic_usb_host_descriptor_parser import DeviceDescriptor
 
-for device in usb.core.find(find_all=True):
+for i, device in enumerate(usb.core.find(find_all=True)):
     device_descriptor = DeviceDescriptor(device)
-    print("# Device")
-    print(device_descriptor)
-    print("## Configurations")
-    for configuration in device_descriptor.configurations:
-        print(configuration)
-        print("### Interfaces")
-        for interface in configuration.interfaces:
-            print(interface)
-            print("#### Endpoints")
-            for endpoint in interface.endpoints:
-                print(endpoint)
+    print(f"Device {i+1}: {device_descriptor}")
+    for j, configuration in enumerate(device_descriptor.configurations):
+        print(f"- Configuration {j+1}: {configuration}")
+        for k, interface in enumerate(configuration.interfaces):
+            print(f"  - Interface {k+1}: {interface}")
+            for l, endpoint in enumerate(interface.endpoints):
+                print(f"    - Endpoints {l+1}: {endpoint}")
+    print()
